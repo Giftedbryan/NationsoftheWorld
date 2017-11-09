f02e370e4b51162d9a574b9c0ff1daa766272427
Giftedbryan <Giftedbryan@LAPTOP-NP50C6C3>
2017-11-09 13:21:19 +0100

    Add project files.

---
+++ NationsoftheWorld/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace NationsoftheWorld
+{
+    public partial class Form1 : Form
+    {
+        bool Finished = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            textBox1.Select();
+            String FileLocation = @"C:\Users\Giftedbryan\Downloads\loads\" + Counters.Nation.ToLower() + ".png";
+            pictureBox1.Image = Image.FromFile(FileLocation);
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Check();
+            #region
+            /*
+            if (textBox1.Text == "The Netherlands")
+            {
+                Counters.Score++;
+                pictureBox1.Image = Image.FromFile(@"C:\Users\Giftedbryan\Downloads\loads\pt.png");
+                //Counters.Nation = "";
+            }
+
+            if (textBox1.Text == "Portugal")
+            {
+                Counters.Score++;
+                pictureBox1.Image = Image.FromFile(@"C:\Users\Giftedbryan\Downloads\loads\be.png");
+                //Counters.Nation = "";
+            }
+
+            if (textBox1.Text == "Belgium")
+            {
+                Counters.Score++;
+                pictureBox1.Image = Image.FromFile(@"C:\Users\Giftedbryan\Downloads\loads\de.png");
+                //Counters.Nation = "";
+            }
+
+            if (textBox1.Text == "Germany")
+            {
+                Counters.Score++;
+                pictureBox1.Image = Image.FromFile(@"C:\Users\Giftedbryan\Downloads\loads\es.png");
+                //Counters.Nation = "";
+            }
+
+            if (textBox1.Text == "Spain")
+            {
+                Counters.Score++;
+                pictureBox1.Image = Image.FromFile(@"C:\Users\Giftedbryan\Downloads\loads\gb.png");
+                //Counters.Nation = "";
+            }
+
+            if (textBox1.Text == "Great Brittain")
+            {
+                Counters.Score++;
+                pictureBox1.Image = Image.FromFile(@"C:\Users\Giftedbryan\Downloads\loads\pl.png");
+                Counters.Nation = "";
+            }
+
+            if (textBox1.Text == "Poland")
+            {
+                Counters.Score++;
+                textBox1.Text = "Good job, you finished the test";
+            }
+            */
+            #endregion Legacy
+
+        }
+        private void Check()
+        {
+            if (Finished == false)
+            {
+
+                if (Program.Checkanswer(textBox1.Text) && !Program.CheckLastNation())
+                {
+                    Counters.Score++;
+                }
+
+                else if (Program.CheckLastNation())
+                {
+                    String FileLocation = @"C:\Users\Giftedbryan\Downloads\loads\gj.png";
+                    pictureBox1.Image = Image.FromFile(FileLocation);
+                    textBox1.Text = "How did you do?";
+                    Finished = true;
+                    textBox1.ReadOnly = true;
+                    pictureBox1.Focus();
+                }
+
+                if (!Program.CheckLastNation())
+                {
+                    Program.UpdateNation();
+                    String FileLocation = @"C:\Users\Giftedbryan\Downloads\loads\" + Counters.Nation.ToLower() + ".png";
+                    pictureBox1.Image = Image.FromFile(FileLocation);
+                    textBox1.Text = "";
+                }
+
+                textBox2.Text = Counters.Score + " out of " + Program.Index;
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+
+
+            if (e.KeyCode == Keys.Enter || e.KeyCode == Keys.Return)
+            {
+                Check();
+            }
+        }
+
+        private void textBox1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter || e.KeyCode == Keys.Return)
+            {
+                Check();
+            }
+        }
+    }
+}
---
+++ NationsoftheWorld/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace NationsoftheWorld
+{
+
+    public class Counters
+    {
+        public static int Score = 0;
+        public static string Nation;
+    }
+
+    static class Program
+    {
+        static Program()
+        {
+            Countryflag.Add("NL");
+            Countryflag.Add("PT");
+            Countryflag.Add("BE");
+            Countryflag.Add("DE");
+            Countryflag.Add("ES");
+            Countryflag.Add("GB");
+            Countryflag.Add("PL");
+            Countryflag.Add("NP");
+
+            Shuffle(Countryflag);
+
+            Counters.Nation = Countryflag[0];
+        }
+
+        public static List<string> Countryflag = new List<string>();
+
+        private static Random rng = new Random();
+
+        public static int Index = 0;
+
+        public static void Shuffle<T>(this IList<T> list)
+        {
+            int n = list.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = rng.Next(n + 1);
+                T value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
+        }
+
+        public static void UpdateNation() {
+            Index++;
+            Counters.Nation = Countryflag[Index];
+        }
+
+        public static bool Checkanswer(String Input)
+        {
+            if (Counters.Nation.ToUpper() == Input.ToUpper())
+            {
+                return true;
+            }
+            else { return false; }
+        }
+
+        public static bool CheckLastNation()
+        {
+            return Index >= Countryflag.Count - 1;
+        }
+
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+
+        }
+    }
+}
